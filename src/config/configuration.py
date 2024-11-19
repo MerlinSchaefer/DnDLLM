353409850427506734c0f355101873a989d33d9b
MerlinSchaefer <ms101196@googlemail.com>
2024-11-19 17:04:30 +0100

    Fix config loading

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -1,17 +1,16 @@
+import os
+
 from dotenv import find_dotenv, load_dotenv
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class DatabaseConfig(BaseSettings):
-    db_name: str
-    host: str
-    password: str
-    port: str
-    user: str
-
-    class Config:
-        env_file = ".env"
-        env_file_encoding = "utf-8"
+    db_name: str = Field(default=os.environ.get("DB_NAME", "default_db"))
+    host: str = Field(default=os.environ.get("HOST", "localhost"))
+    password: str = Field(default=os.environ.get("DB_PASSWORD", "default_password"))
+    port: str = Field(default=os.environ.get("PORT", "5432"))
+    user: str = Field(default=os.environ.get("DB_USER", "default_user"))
 
 
 # Load environment variables
